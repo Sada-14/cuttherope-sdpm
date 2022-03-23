7c441f5353797c00ce0382774e7103a23362f0c0
Sada Siva Tej <51314028+Sada-14@users.noreply.github.com>
2022-03-22 21:58:34 -0400

    Add files via upload

---
+++ Cut_The_Rope_Replica-master/Assets/Scripts/FrogBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FrogBehaviour : MonoBehaviour {
+
+    bool candyCaught = false;
+    public int sceneToLoad;
+
+    void OnTriggerEnter2D (Collider2D other) {
+        if (other.gameObject.tag == "Candy") {
+            other.gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+            other.gameObject.GetComponent<Rigidbody2D> ().isKinematic = true;
+            if (!candyCaught) {
+                candyCaught = true;
+                StartCoroutine (candyDissapear (other.gameObject));
+            }
+        }
+    }
+
+    IEnumerator candyDissapear (GameObject candyObject) {
+        for (float i = 10; i > 0; i--) {
+            candyObject.transform.localScale = new Vector3 (i / 10, i / 10, i / 10);
+            candyObject.transform.position = Vector3.Lerp(candyObject.transform.position, transform.position, 0.1f);
+            yield return new WaitForSeconds (.01f);
+        }
+        Destroy(candyObject);
+        yield return new WaitForSeconds(.8f);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
